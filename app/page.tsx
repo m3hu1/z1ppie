3140006c73f90a165bf83a8d2b706d722455ea5a
m3hu1 <pathakkmehul@gmail.com>
2024-10-16 15:47:50 +0530

    updated navbar and hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { ArrowRight, Info } from "lucide-react";
 import { TypewriterEffectSmooth } from "@/components/ui/typewriter-effect";
+import { GithubIcon } from "lucide-react";
 
 const Home = () => {
   const words = [
@@ -28,15 +28,22 @@ const Home = () => {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center h-[40rem]  ">
+    <div className="flex flex-col items-center justify-center h-[40rem]">
       <TypewriterEffectSmooth words={words} />
       <div className="mt-8 gap-3 flex justify-center">
         <Link href="/share">
-          <Button size="lg">Get Started</Button>
+          <Button size="lg">Start sharing</Button>
         </Link>
-        <Button size="lg" variant={"outline"}>
-          Learn more
+        
+        <Button size="lg" variant={"outline"} className="flex items-center">
+        <Link href={"https://github.com/m3hu1/z1ppie"} className="flex items-center">
+                <GithubIcon size={18} className="mr-2" />
+              
+          GitHub Repo
+        </Link>
         </Button>
+        
+        
       </div>
     </div>
   );
