680fc252165e2c9b70e6e87263da2818463a4034
m3hu1 <pathakkmehul@gmail.com>
2024-10-17 10:23:46 +0530

    updated favicons

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ThemeButton from "./ThemeButton";
 import Image from "next/image";
-import zdark from "@/assets/zdarkfinal.gif";
-import zwhite from "@/assets/zwhite1.gif";
+import rfilled from "@/assets/rfilled.png";
+import rwhite from "@/assets/rwhite.png";
 
 import Link from "next/link";
-// import { LettersPullUp } from "@/components/ui/pullUp";
+import { LettersPullUp } from "@/components/ui/pullUp";
 
 const Navbar = () => {
   return (
@@ -14,17 +14,17 @@ const Navbar = () => {
         <div className="flex justify-center items-center flex-grow">
           <Image
             className="h-8 w-8 p-0 scale-0 hidden dark:flex dark:scale-100"
-            src={zdark}
+            src={rwhite}
             alt="zippy"
           />
           <Image
             className="h-8 w-8 p-0 scale-100 flex dark:scale-0 dark:hidden"
-            src={zwhite}
+            src={rfilled}
             alt="zippyinv"
           />
           <Link href="/" className="ml-2 z-10">
-            z1ppie
-            {/* <LettersPullUp text="z1ppie" /> */}
+            {/* z1ppie */}
+            <LettersPullUp text="z1ppie" />
           </Link>
         </div>
         <div className="flex gap-x-2 z-10">
